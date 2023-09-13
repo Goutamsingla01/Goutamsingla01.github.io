e46f2e2abe9df4fe78f19446a46913ad4ce779fb
Goutam Singla <122777044+Goutamsingla01@users.noreply.github.com>
2023-09-13 17:24:07 +0530

    static folder added

---
+++ static/scroll.js
@@ -0,0 +1,22 @@
+// $('.page-link').click(function() {
+//     var anchor = $(this).attribute('dest');
+//     console.log(anchor)
+//     $('html, body').animate(
+//       {
+//         scrollTop: $('#' + anchor).offset().top
+//       },
+//       400
+//     );
+//   });
+document.getElementsByClassName("page-link")[0].addEventListener("click", displayDate);
+const contact=document.getElementById("contact")
+function displayDate(){
+    var anchor=contact.offsetTop;
+    console.log(anchor)
+    window.scroll({
+        top:anchor,
+        left:0,
+        behavior:"smooth"
+    });
+    
+}
